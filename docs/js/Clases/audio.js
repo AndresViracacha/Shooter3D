4ff32700088a0b3585ae7bfdb94f8fb168ee742b
AndresViracacha <viracfandres@gmail.com>
2020-11-05 10:27:43 -0500

    poniendo el manager para que corra la app hasta que haya cargado la cancion

--- docs/js/Clases/audio.js
+++ docs/js/Clases/audio.js
@@ -2,8 +2,8 @@ function Audio(){
     var listener = new THREE.AudioListener();
     camera.add(listener);
     var sound = new THREE.Audio(listener);
-    var audioLoader = new THREE.AudioLoader();
-    audioLoader.load('audio/UndertaleMegalovania.mp3', function (buffer) {
+    var audioLoader = new THREE.AudioLoader(manager);
+    audioLoader.load('audio/megalovania.mp3', function (buffer) {
         sound.setBuffer(buffer);
         sound.setLoop(true);
         sound.setVolume(0.5);
